ce9db1ebc0516f57c47ef43515fdc4b51c1ea1f6
arthursensai <aytsidibah@gmail.com>
2025-06-11 19:07:38 +0100

    God HELP ME2

--- backend/controllers/profileControllers.js
+++ backend/controllers/profileControllers.js
@@ -7,7 +7,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 //check if the user exits
 const checkUserToken = async (req) => {
     const token = req.cookies.jwt;
-    console.log(token)
+    console.log(req)
 
     if (!token) { return null };
     
