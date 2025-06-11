5ce8ade1316b6c04991072538d2fa9a668ab7353
arthursensai <aytsidibah@gmail.com>
2025-06-11 14:25:45 +0100

    Current Music Player

--- frontend/src/Components/MusicPanel.jsx
+++ frontend/src/Components/MusicPanel.jsx
@@ -1,13 +1,14 @@
 import { Music } from 'lucide-react';
+import MusicPlayer from './MusicPlayer';
 
 const MusicPanel = () => {
     return (
-        <div className="bg-surface rounded-2xl p-6 border border-primary/20 w-1/4">
+        <div className="flex flex-col bg-surface rounded-2xl p-6 border border-primary/20 w-1/4 gap-4">
             <div className="flex gap-2 text-center">
                 <Music className="text-accent text-center" size={36}/>
                 <h1 className="text-2xl">Music Panel</h1>
             </div>
-            <p>Coming Soon</p>
+            <MusicPlayer />
         </div>
     )
 };
