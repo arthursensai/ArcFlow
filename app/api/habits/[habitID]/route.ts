ab5570a902bb7c6f3a528ec493cb93ada5f04dc6
arthursensai <aytsidibah@gmail.com>
2025-08-24 17:35:17 +0100

    what about now?

--- app/api/habits/[habitID]/route.ts
+++ app/api/habits/[habitID]/route.ts
@@ -4,10 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  context: { params: { habitID: string[] } }
+  { params }: { params: { habitID: string } }
 ) {
-  const params = await context.params;
-  const habitID = params.habitID[0];
+  const { habitID } = await params;
 
   const session = await auth();
 
